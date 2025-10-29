073f65e88ad0e8bee93c471faf10e3633a74f5b8
Amr Saleh <amrsalehma@gmail.com>
2025-10-29 19:50:46 +0300

    add background to screen

--- Mobile/lib/presentation/screens/upload_profile_photo_screen/upload_profile_photo_screen.dart
+++ Mobile/lib/presentation/screens/upload_profile_photo_screen/upload_profile_photo_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../../resources/color_manager.dart';
+import '../../resources/image_manager.dart';
+
 class UploadProfilePhotoScreen extends StatefulWidget {
   const UploadProfilePhotoScreen({super.key});
   static String id = 'UploadProfilePhotoScreen';
@@ -11,6 +14,40 @@ class UploadProfilePhotoScreen extends StatefulWidget {
 class _UploadProfilePhotoScreenState extends State<UploadProfilePhotoScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: ColorManager.backgroundColor,
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          SizedBox(
+            width: double.infinity,
+            height: double.infinity,
+            child: Image.asset(
+              ImageManager.loginAndSignupBackgroundImage,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned(
+            child: Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height * 0.68,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50),
+                  topRight: Radius.circular(50),
+                ),
+                color: ColorManager.white,
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  children:[],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
