58564d31da554d9f128e02ef072ee6e19ec9abc1
Amr Saleh <amrsalehma@gmail.com>
2025-11-01 16:52:42 +0200

    solve keyboard issue

--- Mobile/lib/presentation/screens/signup_screen/signup_screen.dart
+++ Mobile/lib/presentation/screens/signup_screen/signup_screen.dart
@@ -23,9 +23,10 @@ class SignupScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Object? object = ModalRoute.of(context)!.settings.arguments;
-    File? image = object == null? null : object as File;
+    File? image = object == null ? null : object as File;
     return Scaffold(
       backgroundColor: ColorManager.backgroundColor,
+      resizeToAvoidBottomInset: false,
       body: Stack(
         alignment: Alignment.bottomCenter,
         children: [
@@ -35,117 +36,125 @@ class SignupScreen extends StatelessWidget {
             child: Image.asset(ImageManager.BackgroundImage, fit: BoxFit.cover),
           ),
           Positioned(
-            child: Container(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height * 0.68,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(50),
-                  topRight: Radius.circular(50),
+            child: SingleChildScrollView(
+              child: Container(
+                width: double.infinity,
+                height:
+                    MediaQuery.of(context).size.height * 0.68 +
+                    MediaQuery.of(context).viewInsets.bottom,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50),
+                  ),
+                  color: ColorManager.white,
                 ),
-                color: ColorManager.white,
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0),
-                child: Column(
-                  children: [
-                    SizedBox(height: 42),
-                    Text(
-                      'Get Started',
-                      style: TextStyle(
-                        color: ColorManager.primaryTextColor,
-                        fontSize: 32,
-                        fontWeight: FontWeight.w700,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    left: 20.0,
+                    right: 20.0,
+                    bottom: MediaQuery.of(context).viewInsets.bottom,
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 42),
+                      Text(
+                        'Get Started',
+                        style: TextStyle(
+                          color: ColorManager.primaryTextColor,
+                          fontSize: 32,
+                          fontWeight: FontWeight.w700,
+                        ),
                       ),
-                    ),
-                    Spacer(flex: 3),
-                    CustomTextField(
-                      labelText: 'Full name',
-                      hintText: 'Entre username',
-                      controller: userNameController,
-                    ),
-                    SizedBox(height: 16),
-                    CustomTextField(
-                      labelText: 'Email',
-                      hintText: 'Entre email',
-                      controller: emailController,
-                    ),
-                    SizedBox(height: 16),
-                    CustomTextField(
-                      labelText: 'Password',
-                      hintText: 'Entre Password',
-                      controller: passwordController,
-                      isSecureText: true,
-                      obscureText: true,
-                    ),
-                    SizedBox(height: 16),
-                    CustomTextField(
-                      labelText: 'Confirm password',
-                      hintText: 'Entre Password again',
-                      controller: confirmPasswordController,
-                      isSecureText: true,
-                      obscureText: true,
-                    ),
-                    Spacer(flex: 7),
-                    CustomButton(
-                      text: 'Signup',
-                      isWhite: false,
-                      isTransparent: false,
-                      isPrimaryTextColor: false,
-                      onTap: () async {
-                        log(
-                          'email: ${emailController.text} \n password: ${passwordController.text}',
-                        );
-                        String token = await SignupRequest().signupRequest(
-                          userName: userNameController.text,
-                          email: emailController.text,
-                          imageFile: image,
-                          password: passwordController.text,
-                          confirmPassword: confirmPasswordController.text,
-                        );
-                        EyeRecognition.token = token;
-                        log(token);
-                        if (EyeRecognition.success == true) {
-                          Navigator.pushReplacementNamed(
-                            context,
-                            HomeScreen.id,
+                      Spacer(flex: 3),
+                      CustomTextField(
+                        labelText: 'Full name',
+                        hintText: 'Entre username',
+                        controller: userNameController,
+                      ),
+                      SizedBox(height: 16),
+                      CustomTextField(
+                        labelText: 'Email',
+                        hintText: 'Entre email',
+                        controller: emailController,
+                      ),
+                      SizedBox(height: 16),
+                      CustomTextField(
+                        labelText: 'Password',
+                        hintText: 'Entre Password',
+                        controller: passwordController,
+                        isSecureText: true,
+                        obscureText: true,
+                      ),
+                      SizedBox(height: 16),
+                      CustomTextField(
+                        labelText: 'Confirm password',
+                        hintText: 'Entre Password again',
+                        controller: confirmPasswordController,
+                        isSecureText: true,
+                        obscureText: true,
+                      ),
+                      Spacer(flex: 7),
+                      CustomButton(
+                        text: 'Signup',
+                        isWhite: false,
+                        isTransparent: false,
+                        isPrimaryTextColor: false,
+                        onTap: () async {
+                          log(
+                            'email: ${emailController.text} \n password: ${passwordController.text}',
                           );
-                        }
-                      },
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 20.0, bottom: 60.0),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            'Already i have an account? ',
-                            style: TextStyle(
-                              color: ColorManager.secondTextColor,
-                              fontSize: 14,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.pushReplacementNamed(
-                                context,
-                                LoginScreen.id,
-                              );
-                            },
-                            child: Text(
-                              ' Login',
+                          String token = await SignupRequest().signupRequest(
+                            userName: userNameController.text,
+                            email: emailController.text,
+                            imageFile: image,
+                            password: passwordController.text,
+                            confirmPassword: confirmPasswordController.text,
+                          );
+                          EyeRecognition.token = token;
+                          log(token);
+                          if (EyeRecognition.success == true) {
+                            Navigator.pushReplacementNamed(
+                              context,
+                              HomeScreen.id,
+                            );
+                          }
+                        },
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 20.0, bottom: 60.0),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              'Already i have an account? ',
                               style: TextStyle(
-                                color: ColorManager.primaryTextColor,
+                                color: ColorManager.secondTextColor,
                                 fontSize: 14,
                                 fontWeight: FontWeight.w700,
                               ),
                             ),
-                          ),
-                        ],
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.pushReplacementNamed(
+                                  context,
+                                  LoginScreen.id,
+                                );
+                              },
+                              child: Text(
+                                ' Login',
+                                style: TextStyle(
+                                  color: ColorManager.primaryTextColor,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
